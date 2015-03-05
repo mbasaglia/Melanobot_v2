8cb30fa9c2f0d44bf8124e34aa93914419003310
Mattia Basaglia <mattia.basaglia@gmail.com>
2015-03-05 10:35:34 +0100

    More documentation tweaks

--- commit-doc.sh
+++ commit-doc.sh
@@ -2,8 +2,10 @@
 
 if [ -n "$1" -a "$(basename "$(git -C "$1" rev-parse --show-toplevel)")" = html ]
 then
-    git -C "$1" commit -am "Update from Doxygen"
-    git -C "$1" push origin gh-pages
+    cd "$1"
+    git add .
+    git commit -am "Update from Doxygen"
+    git push origin gh-pages
 else
     echo "$1 isn't a git repository"
 fi
